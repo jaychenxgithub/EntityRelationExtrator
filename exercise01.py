03c469775ac891c9a30dc2dbffcd52d239fa055e
jaychenxgithub <jaychen.jc@google.com>
2019-12-30 19:29:34 +0800

    branch dev 張學友

--- exercise01.py
+++ exercise01.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from pyltp import SentenceSplitter
 #sents = SentenceSplitter.split('元芳你怎么看？我就趴窗口上看呗！')  # 分句
-sents = SentenceSplitter.split('張學友你怎么看？我就趴窗口上看呗！')  # 分句
+#sents = SentenceSplitter.split('張學友你怎么看？我就趴窗口上看呗！')  # 分句
+sents = SentenceSplitter.split('checkout -b dev: 張學友你怎么看？我就趴窗口上看呗！')  # 分句
+
 
 print ('\n'.join(sents))
